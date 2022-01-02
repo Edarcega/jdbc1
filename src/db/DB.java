a0fcd56cd47316924d7437968624e35512c351ab
Edarcega <edimar.eds@gmail.com>
2022-01-02 12:37:10 -0300

    Projeto apenas conecta em um banco de dados

---
+++ src/db/DB.java
@@ -0,0 +1,58 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DB {
+
+	private static Connection conn = null;
+
+	/* Conectar com o Banco de dados é 
+	 * instanciar um objeto do tipo Connections
+	*/
+	public static Connection getConnection() {
+		if (conn == null) {
+			try {
+				Properties props = loadProperties();
+				String url = props.getProperty("dburl");
+				conn = DriverManager.getConnection(url, props);
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+
+		}
+
+		return conn;
+	}
+
+	/*
+	 *O SQL Exception é derivado de exception que somos obrigados a tratar
+	 *por isso criamos o dbException, para que o tratamento seja
+	 *efetuado quando julgarmos necessário. 
+	 */
+	public static void closeConnection() {
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+
+		}
+	}
+
+	private static Properties loadProperties() {
+		try (FileInputStream fs = new FileInputStream("db.properties")) {
+			Properties props = new Properties();
+			props.load(fs);
+			return props;
+		} catch (IOException e) {
+			throw new DbException(e.getMessage());
+		}
+	}
+
+}
